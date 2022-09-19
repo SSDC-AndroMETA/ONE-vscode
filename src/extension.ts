a46c8287e4d7bfec10d0d5cf112e9e13ead95b81
chshin59 <97646064+chshin59@users.noreply.github.com>
2022-09-19 15:17:41 +0900

    [Relation Viewer] Init relation viewer (#27)
    This commit initiates relation viewer.
    
    ONE-vscode-DCO-1.0-Signed-off-by: chshin59 <chshin59@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -27,6 +27,7 @@ import {PartEditorProvider} from './PartEditor/PartEditor';
 import {PartGraphSelPanel} from './PartEditor/PartGraphSelector';
 import {ToolchainProvider} from './Toolchain/ToolchainProvider';
 import {MetadataViewerPanel} from './MetadataViewer/MetadataViewerPanel';
+import {RelationViewerPanel} from './RelationViewer/RelationViewerPanel';
 import {Logger} from './Utils/Logger';
 
 import {MetadataEventManager} from './MetadataManager/MetadataEventManager';
@@ -72,6 +73,8 @@ export function activate(context: vscode.ExtensionContext) {
   CircleViewerProvider.register(context);
 
   MetadataViewerPanel.register(context);
+
+  RelationViewerPanel.register(context);
 
   // returning backend registration function that will be called by backend extensions
   return backendRegistrationApi();

b54f39302f7d37c05ed2409cc2057342ff3cb83c
root <root@hm.4t4hclpmegouxmcq0lkdmmdanc.syx.internal.cloudapp.net>
2022-09-20 02:00:58 +0000

    Metadata Manager] create test tool
    commit create this test tool

--- src/Metaprovider/metadata_manager.ts
+++ src/Metaprovider/metadata_manager.ts
@@ -1,5 +1,24 @@
 import * as vscode from 'vscode';
 
+interface Relation{
+    "selected": string,
+    "relationData": Node[]
+}
+
+interface Node{
+    "id": string,
+    "parent": string,
+    "represent": number,
+    "dataList": Data[]
+}
+
+interface Data{
+    "path": string,
+    "name": string,
+    "onecc_version"?: string,
+    "toolchain_version"?: string
+}
+
 export class Metadata{
 
     private _disposables: vscode.Disposable[] = [];
@@ -9,7 +28,8 @@ export class Metadata{
             vscode.commands.registerCommand('one.metadata.showMetadata', async () => {
 
                 const testPath :string = "./model.tflite" // workspace 기준 실제 파일 위치
-                await Metadata.getMetadata(context, testPath);
+                // await Metadata.getMetadata(context, testPath);
+                await Metadata.getRelation(context, testPath);
             })
         ]
 
@@ -37,6 +57,67 @@ export class Metadata{
             console.log(JSON.parse(metadata));
             return metadata;
         }); 
+
+    }
+
+    public static async getRelation(context: vscode.ExtensionContext, uri: string) {
+        //uri를 통해 hash값 가져오는 로직 필요
+
+        const nowHash = "9f8641056d4e2eb03830f3c1bbb6c71ca6e820f6da94bf7055b132b8d2e6a2b5"
+
+        // relation.json 불러오기
+    
+        let relationUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri,".meta/relation.json")
+        let relationJSON: JSON = JSON.parse(Buffer.from(await vscode.workspace.fs.readFile(relationUri)).toString())
+        
+        console.log(relationJSON)
+        // 반환 객체 생성
+        let relations: Relation = {
+            "selected" :  "",
+            "relationData" : []
+        }
+
+        // 현재 노드 메타데이터 불러오기
+
+        const metaUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, `.meta/hash_objects/${nowHash.substring(0, 2)}/${nowHash.substring(2)}.json`);
+        let nowMetadata: JSON = JSON.parse(Buffer.from(await vscode.workspace.fs.readFile(metaUri)).toString())
+
+        console.log(nowMetadata)
+
+        relations.selected = nowHash
+        let dataList: Data[] =[]
+        let keys = Object.keys(nowMetadata)
+        for (let i = 0; i < keys.length; i++){
+            let element = nowMetadata[keys[i]]
+            let data: Data = {
+                "path": element.path,
+                "name": element.name,
+                "onecc_version": element.onecc_version,
+                "toolchain_version": element.toolchain_version
+            }
+
+            dataList.push(data);
+        }
+
+        console.log(dataList)
+
+        relations.relationData.push({ "id": nowHash, "parent": relationJSON[nowHash].parent, "represent": 0, "dataList": dataList })
+        
+        console.log(relations)
+        
+
+        // 부모 노드 찾기
+        let tempHash = relationJSON[nowHash].parent
+        // while (true) {
+
+        //     if (tempHash == "") {
+        //         break;
+        //     }
+        // }
+
+        // 자식 노드 찾기
+
+
 
     }
 }
